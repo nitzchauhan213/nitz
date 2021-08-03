30fcb2126e8e2f3eb2bb4deb1b39e005196fff6d
Admin <Admin@DESKTOP-H3GKQ2H>
2021-08-03 14:55:47 +0530

    pushingCode

---
+++ pkg/src/pkg/lp.java
@@ -0,0 +1,21 @@
+package pkg;
+
+public class lp
+{
+
+	public static void main(String[] args) 
+	
+	{
+		int a=0;
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{ 
+				a=a+1;
+				System.out.print(a+"");
+			}
+			System.out.println();
+		}
+		
+	}
+}
